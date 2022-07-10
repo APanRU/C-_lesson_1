bd5385cc57a1cd6263a823feef653b828aec74c9
APanRU <eklmn2004@mail.ru>
2022-07-10 12:45:35 +0300

    update_prog_random

--- lesson_1/Sum/Program.cs
+++ lesson_1/Sum/Program.cs
@@ -1,7 +1,9 @@
-﻿// int numberA = 3;
-// int numberB = 5;
-// Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
 
- double numberA = 12;
- double numberB = 5;
- Console.WriteLine(numberA / numberB);
+ // double numberA = 12;
+ // double numberB = 5;
+ // Console.WriteLine(numberA / numberB);
