a559bee02a6e60520bcef8c8975ed646d0965396
APanRU <eklmn2004@mail.ru>
2022-07-10 13:15:20 +0300

    add_task_1

---
+++ lesson_1/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 4;
+int c = 18;
+int d = 11;
+int e = 22;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
